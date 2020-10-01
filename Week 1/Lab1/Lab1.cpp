07cc53633dfa386d5d87038c3350ef61b3d2d4b7
ngmeijer <meijernils9@gmail.com>
2020-10-01 17:41:07 +0200

    Found lab1 code back

--- Week 1/Lab1/Lab1.cpp
+++ Week 1/Lab1/Lab1.cpp
@@ -1,104 +1,140 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <vector>
 
 #define ARRAY_SIZE 5
 
 using namespace std;
 
 const int MAX = 3;
-int numbersArray[MAX] = { };
-
+int numbersArray[MAX] = { 0 };
 int* currentArrayNumber;
-int lastInt = 0;
 
-void debugLine(const char* testString, int exampleNumber, const char* testString2) {
-	std::cout << testString << exampleNumber << testString2;
-	return;
-}
+std::vector<string> stringArray;
+std::vector<char> firstLetters;
 
-void doubleInput() {
-	cout << "GIVE VALUE BITCH! : ";
-
-	int valueGiven;
-	cin >> valueGiven;
-
-	int result = valueGiven * 2;
-	cout << result;
-}
-
-void inputArrayElementCin() {
+void inputIntArrayElementCin() {
 	//User input
 	for (int i = 0; i < MAX; i++) {
 		cin >> numbersArray[i];
 	}
 }
 
-void inputArrayElementScanf() {
+void inputIntArrayElementScanf() {
 	//User input
 	for (int i = 0; i < MAX; i++) {
 		scanf_s("%d", &numbersArray[i]);
 	}
 }
 
-void sortArrayElements() {
-	for (int index = 0; index < MAX; index++) {
+void inputStringArrayElementCin() {
+	//User input
+	string input;
 
-		greaterThan(lastInt, numbersArray[index]);
-
-		if (greaterThan(lastInt, numbersArray[index]) == true) {
-			numbersArray[index] = MAX;
-		}
+	while (cin >> input && input != "quit" && input.length() <= 30) {
+		stringArray.push_back(input);
 	}
 }
 
-bool greaterThan(int a, int b) {
-	return a > b;
+
+struct SwapElementValues {
+	int elementA;
+	int elementB;
+};
+
+SwapElementValues swapElements(int elementA, int elementB) {
+	SwapElementValues elements = { elementA,elementB };
+	int tempElement = elementA;
+	elementA = elementB;
+	elementB = tempElement;
+
+	return elements;
 }
 
-void printArrayElements() {
+void sortIntArrayElements() {
+	std::vector<int> intVec(numbersArray, numbersArray + MAX);
+
+	std::sort(intVec.begin(), intVec.end(), std::greater<int>());
+
+	std::cout << "The array with sorted numbers contains:";
+	for (std::vector<int>::iterator it = intVec.begin(); it != intVec.end(); ++it)
+		std::cout << ' ' << *it;
+	std::cout << '\n';
+}
+
+bool comparer(string a, string b)
+{
+	return a < b;
+}
+
+void sortStringDynArrayElements() {
+	cout << "\n" << "The strings will now be sorted alphabetically, based on the first letter of the string." << endl;
+
+	int firstLetterArraySize = firstLetters.size();
+
+	sort(stringArray.begin(), stringArray.end());
+
+	//int stringArraySize = stringArray.size();
+	//std::vector<string> stringVec(stringArray, stringArray.operator=);
+
+	//std::sort(stringVec.begin(), stringVec.end(), std::greater<string>());
+}
+
+
+
+void printIntArrayElements() {
 	//Output
-	cout << "\n" << "Implementation with c.out with pointers. Comment and uncomment desired input method." << endl;
+	//cout << "\n" << "Implementation with c.out with pointers. Comment and uncomment desired input method." << endl;
+	cout << "\n";
 	for (int i = 0; i < MAX; i++) {
 
-		cout << "Value of array elements [" << i << "] = ";
+		cout << "Value of int array elements [" << i << "] = ";
 
 		//Get array element address (pointer)
 		currentArrayNumber = &numbersArray[i];
 
-		////Convert to int with * and write console line out.
+		//Convert to int with * and write console line out.
 		cout << (*currentArrayNumber) << endl;
 	}
 
-	cout << "\n" << "Implementation with scanf and printf" << endl;
-	for (int i = 0; i < MAX; i++) {
+	//cout << "\n" << "Implementation with scanf and printf" << endl;
+	//for (int i = 0; i < MAX; i++) {
 
-		//Get array element address (pointer)
-		currentArrayNumber = &numbersArray[i];
-		//Convert to int with * and print line
-		printf("The values of array = %u\n", *currentArrayNumber);
+	//	//Get array element address (pointer)
+	//	currentArrayNumber = &numbersArray[i];
+	//	//Convert to int with * and print the line
+	//	printf("The values of array = %u\n", *currentArrayNumber);
+	//}
+}
+
+void printStringArrayElements() {
+	cout << "\n";
+	for (string& givenString : stringArray) {
+		cout << givenString << endl;
 	}
 }
 
 int main()
 {
 	cout << "\n" << "GIMME NUMBERS: " << endl;
+	inputIntArrayElementCin();
+	//inputIntArrayElementScanf();
 
-	inputArrayElementCin();
-	//inputArrayElementScanf();
-	printArrayElements();
+	cout << "\n" << "NOW GIMME WORDS" << endl;
+	inputStringArrayElementCin();
 
-	sortArrayElements();
+	
+	cout << "\nRESULTS OF EVERYTHING" << endl;
+	printIntArrayElements();
+	printStringArrayElements();
 
-	cout << "\n" << "The numbers will now be sorted from high to low." << endl;
+	sortIntArrayElements();
 
-	printArrayElements();
+	sortStringDynArrayElements();
 
-	//Memory freeing causes a breakpoint to be triggered by VS, and the program to crash!
+	printStringArrayElements();
+
+	//Memory freeing causes an internal breakpoint to be triggered by VS, and the program to crash!
 	//delete[] numbersArray;
 }
-
-
-/////////////////
-//https://stackoverflow.com/questions/10865957/printf-with-stdstring 
-//Ask this in next lab; explanation about preference for printf over std::cout << "".
-/////////////////
