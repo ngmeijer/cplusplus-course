941fd17515d62507d97842f3c3b3ccef02a50cc9
ngmeijer <meijernils9@gmail.com>
2020-10-01 16:27:06 +0200

    Multiple sprites

--- Week2/SFML environment/SFML_visual/main.cpp
+++ Week2/SFML environment/SFML_visual/main.cpp
@@ -10,23 +10,6 @@
 #define KEY_LEFT 75
 #define KEY_RIGHT 77
 
-using namespace std;
-
-float xPosition;
-float yPosition;
-
-float moveSpeed = 0.1f;
-
-bool movingLeft = false;
-bool movingRight = false;
-bool movingUp = false;
-bool movingDown = false;
-
-sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML_visual");
-sf::Texture texture;
-const int MAX_SPRITES = 2;
-PlayerSprite spriteArray[MAX_SPRITES];
-int randomXosition;
 
 class PlayerSprite
 {
@@ -39,26 +22,57 @@ public:
 	PlayerSprite(int x, int y, sf::Sprite sprite) {
 		_xPosition = x;
 		_yPosition = y;
+		_sprite = sprite;
 		texture.loadFromFile("PixelTexture.png");
+		sprite.setTexture(texture);
+	}
+
+	void setPosition(int xPosition, int yPosition) {
+		_sprite.setPosition(xPosition, yPosition);
 	}
 };
 
+float xPositionArray;
+float yPosition;
+
+float moveSpeed = 0.1f;
+
+bool movingLeft = false;
+bool movingRight = false;
+bool movingUp = false;
+bool movingDown = false;
+
+sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML_visual");
+const int MAX_SPRITES = 6;
+
+int randomXosition;
+
+using namespace std;
+
+sf::Texture texture;
+sf::Sprite sprite;
+int xPositionsArray[MAX_SPRITES];
+int yPositionsArray[MAX_SPRITES];
+vector<PlayerSprite> spriteArray;
+
 int main()
 {
-	srand(time(0));
+	window.clear();
+	texture.loadFromFile("PixelTexture.png");
+	sprite.setTexture(texture);
 	for (int CURRENT_SPRITE = 0; CURRENT_SPRITE < MAX_SPRITES; CURRENT_SPRITE++)
 	{
-		xPosition = rand() % 900 + 100;
+		int xPosition = rand() % window.getSize().x - 100;
+		xPositionsArray[CURRENT_SPRITE] = xPosition;
 
-		spriteArray[CURRENT_SPRITE].
-			spriteArray[CURRENT_SPRITE].setPosition(((window.getSize().y) / 2), (window.getSize().y) / 2);
-		xPosition = spriteArray[CURRENT_SPRITE].getPosition().x;
-		yPosition = spriteArray[CURRENT_SPRITE].getPosition().y;
+		int yPosition = rand() % window.getSize().y - 100;
+		yPositionsArray[CURRENT_SPRITE] = yPosition;
 
 		cout << xPosition << endl;
+		spriteArray.push_back(PlayerSprite(200, 50, sprite));
+		spriteArray[CURRENT_SPRITE].setPosition(xPositionsArray[CURRENT_SPRITE], yPositionsArray[CURRENT_SPRITE]);
+		window.draw(spriteArray[CURRENT_SPRITE]._sprite);
 	}
-
-
 
 	while (window.isOpen())
 	{
@@ -69,26 +83,38 @@ int main()
 				window.close();
 		}
 
+		window.display();
+
 		for (int CURRENT_SPRITE = 0; CURRENT_SPRITE < MAX_SPRITES; CURRENT_SPRITE++) {
-
-			window.clear();
-			window.draw(spriteArray[CURRENT_SPRITE]);
-			window.display();
-
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-				spriteArray[CURRENT_SPRITE].setPosition(xPosition -= moveSpeed, yPosition);
+				spriteArray[CURRENT_SPRITE].setPosition(xPositionArray -= moveSpeed, yPosition);
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-				spriteArray[CURRENT_SPRITE].setPosition(xPosition += moveSpeed, yPosition);
+				spriteArray[CURRENT_SPRITE].setPosition(xPositionArray += moveSpeed, yPosition);
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-				spriteArray[CURRENT_SPRITE].setPosition(xPosition, yPosition += moveSpeed);
+				spriteArray[CURRENT_SPRITE].setPosition(xPositionArray, yPosition += moveSpeed);
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-				spriteArray[CURRENT_SPRITE].setPosition(xPosition, yPosition -= moveSpeed);
+				spriteArray[CURRENT_SPRITE].setPosition(xPositionArray, yPosition -= moveSpeed);
 
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+			/*if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
 				spriteArray[CURRENT_SPRITE].setColor(sf::Color::Transparent);
 			}
 			else
-				spriteArray[CURRENT_SPRITE].setColor(sf::Color::Red);
+				sprite.setColor(sf::Color::Red);
+
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+					spriteArray[CURRENT_SPRITE].setPosition(xPosition -= moveSpeed, yPosition);
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+					spriteArray[CURRENT_SPRITE].setPosition(xPosition += moveSpeed, yPosition);
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+					spriteArray[CURRENT_SPRITE].setPosition(xPosition, yPosition += moveSpeed);
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+					spriteArray[CURRENT_SPRITE].setPosition(xPosition, yPosition -= moveSpeed);
+
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+					spriteArray[CURRENT_SPRITE].setColor(sf::Color::Transparent);
+				}
+				else
+					spriteArray[CURRENT_SPRITE].setColor(sf::Color::Red);*/
 		}
 	}
 
